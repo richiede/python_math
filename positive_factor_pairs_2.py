ad5b4122df5fd6eae05551dd0661855eca86237b
richiede <68747135+richiede@users.noreply.github.com>
2022-11-09 21:03:11 +0000

    Create positive_factor_pairs_2.py

---
+++ positive_factor_pairs_2.py
@@ -0,0 +1,14 @@
+# Update positive fator pairs
+# Takes a number "num" and finds all positive factor pairs
+
+final_list = []
+
+num = 124
+
+for i in range(1, num+1):
+    if num % i == 0:
+        temp_list = sorted([int(num/i), int(i)])
+        if temp_list not in final_list:
+            final_list.append(temp_list)
+    return final_list
+  
